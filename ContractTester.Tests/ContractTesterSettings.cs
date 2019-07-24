79805c06f453fb30b0d1a57336186de8ad6b2eb8
Kiet Huynh <kiet.huynh@headspring.com>
2019-07-24 14:44:29 -0500

    Add OrderCompleted command tests as another command to test against.

--- ContractTester.Tests/ContractTesterSettings.cs
+++ ContractTester.Tests/ContractTesterSettings.cs
@@ -4,5 +4,6 @@
     {
         public string TestMessageApiUrl { get; set; }
         public string ValidPlaceOrderContractId { get; set; }
+        public string ValidOrderCompletedContractId { get; set; }
     }
 }
